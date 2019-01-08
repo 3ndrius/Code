47e378488a71e85101dae478fcbb8e545835e360
3ndrius <andrzejo1991@gmail.com>
2019-01-08 22:37:00 +0100

    Refresh

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div className="App">
+
+      
+      </div>
+    );
+  }
+}
+
+export default App;
