ad6e3ac7ad0906e0226cb25fe576e67802ed3f9f
3ndrius <andrzejo1991@gmail.com>
2019-01-09 16:51:22 +0100

    Getting data from api destruct and render on page, draft styled-comp

--- src/App.js
+++ src/App.js
@@ -4,41 +4,57 @@ import styled from 'styled-components'
 
 const Wrap = styled.div`
     max-width:1200px;
-    min-height:200px;
+    height:100%;
+    padding:10px;
     margin:0 auto;
     display:flex;
-    justify-content:center;
-    
+    align-content:center; 
 `;
-
+const Content = styled.div`
+  display:grid;
+  grid-template-columns:150px 200px;
+  grid-template-rows:auto;
+  align-items:center;
+`;
 const SearchBox = styled.input`
-  height:50px;
-  width:250px;
-  padding:0;
-  margin:0;
-  border:.1px solid black;
-  font-size:1.2em;
+border: 1px solid #d1d1d1;
+font-size: 1.2em;
+width: 200px;
+height:54px;
+padding:0px 5px;
+margin: 0 0 15px 0;
+box-sizing:border-box;
+
 `;
 const Header = styled.h1`
-  
   font-size:3em;
 `;
+const Caption = styled.span`
+  font-size:1em;
+  grid-column-start:1;
+  grid-column-end:2;
+  padding:10px 0;
+`;
+const Paragraph = styled.p`
+  font-size:1em;
+  padding:10px 0;
+  margin:0;
+`;
 const Btn = styled.button`
-  border:1px solid black;
-  border-left:none;
-  height:50px;
+  border: 1px solid #d1d1d1;
+  height:54px;
+  margin-left:5px;
   width:100px;
   cursor:pointer;
   background:white;
   font-size:1em;
   
 `;
-const List = styled.ul`
-  list-style-type:none;
-  font-size:1.2em;
-`;
 const Figure = styled.img`
-
+  grid-column-start:1;
+  grid-column-end:3;
+  grid-row-start:1;
+  grid-rows-end:2;
 `;
 class App extends Component {
 
@@ -64,12 +80,9 @@ class App extends Component {
       value: e.target.value
     })
   }
-
-
-
   render() {
-    console.log(this.state.user.message);
-    const {login,avatar_url, blog, following, followers, gists_url,bio } = this.state.user;
+    console.log(this.state.user);
+    const {login,avatar_url, blog, following, followers, gists_url,bio, company, created_at, hireable, html_url, location, name,subscriptions_url} = this.state.user;
     return (
       <div className="App">
         <Wrap>
@@ -80,28 +93,28 @@ class App extends Component {
           <Btn onClick={this.getUser}>Search</Btn>
         </Wrap>
         <Wrap>
-          
           {
-          ( this.state.user.message) ? this.state.user.message :
+          ( this.state.user.message) ? "User not found" :
             this.state.stat ?
-            <List>
-              <li><span>Login: </span> {login}</li>
-              <li><span>Blog_url: </span>{blog ? blog : "Not fill"}</li>
-              <li>{following}</li>
-              <li>{followers}</li>
-              <li>{bio}</li>
-              <li><Figure src={avatar_url} alt="avatar" width="100px" height="auto"/></li>
-            </List>
+              <Content>
+                <Figure src={avatar_url} alt="avatar" height="auto" width="300px"/><span></span>
+                <Caption>Name:</Caption> <Paragraph>{name}</Paragraph>
+                <Caption>Login:</Caption> <Paragraph>{login}</Paragraph>
+                <Caption>Location:</Caption> <Paragraph>{location}</Paragraph>
+                <Caption>Created_at:</Caption> <Paragraph>{created_at}</Paragraph>
+                <Caption>Blog_url:</Caption> <Paragraph>{blog}</Paragraph>
+                <Caption>Gists_url:</Caption> <Paragraph>{gists_url}</Paragraph>
+                <Caption>Following:</Caption> <Paragraph>{following}</Paragraph>
+                <Caption>Followers:</Caption> <Paragraph>{followers}</Paragraph>
+                <Caption>Bio:</Caption> <Paragraph>{bio}</Paragraph>
+                <Caption>Subscription:</Caption> <Paragraph>{subscriptions_url}</Paragraph>
+              </Content>
             :
             <span>No user yet...</span>
-        
           }
-
-        </Wrap>
-      
+        </Wrap>  
       </div>
     );
   }
 }
-
 export default App;
